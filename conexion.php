e1fc37792d09c8a2fab1c86c9d247860ab66277a
Polo The Noob <marr40201@gmail.com>
2023-09-04 21:44:44 -0600

    Archivo Conexion

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+   $servername = "localhost";
+   $user = "root";
+   $password = "12345678";
+   $dbname = "base_final";
+   $conex = new mysqli($servername,$user,$password,$dbname);
+
+    
+?>
--- index.php
+++ index.php
@@ -1,2 +1,24 @@
 <?php
+    require("conexion.php");
     echo "Ya duermanlo como  a los perritos";
+    echo "Proyecto global";
+
+    $query = "SELECT * FROM detalle_venta";
+    if ($result = $conex->query($query)) {
+
+		while ($row = $result->fetch_assoc()) {
+			$campo =$row["id_producto"];
+            $campo1 =$row["id_cliente"];
+            $id_prove = $row["id_provedor"];
+            $fecha = $row["fecha"];
+    		echo $campo. " " . $campo1;
+            echo $id_prove;
+            echo $fecha."<br>";
+            
+		}
+
+	/*freeresultset*/
+	$result->free();
+    }
+
+?>
