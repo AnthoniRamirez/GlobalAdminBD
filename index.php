5c94b2b56b8d9f65fd4b872848191953f605c088
Polo The Noob <marr40201@gmail.com>
2023-09-05 21:37:53 -0600

    JOINS De la tabla detalle_venta

--- index.php
+++ index.php
@@ -1,19 +1,44 @@
 <?php
     require("conexion.php");
-    echo "Ya duermanlo como  a los perritos";
-    echo "Proyecto global";
+    echo "Ya duermanlo como  a los perritos"."<br>";
+    echo "Proyecto global"."<br>";
 
     $query = "SELECT * FROM detalle_venta";
-    if ($result = $conex->query($query)) {
+    $query1 = "SELECT det.id_pedido,cli.nombre as nombrecliente, cli.apellido,
+    cli.colonia AS calleCLI, cli.calle AS coloniaCLI, cli.estado AS EDOcli, cli.telefono AS celCLI,
+    cli.correo AS MAILcli, det.fecha, prod.nombre AS NOMpro, pro.rfc_provedor, pro.nombre as nombreproveedor, 			pro.colonia AS coloniaPRO,
+    pro.calle AS callePRO, pro.estado AS EDOpro, pro.telefono AS CELpro
+    FROM detalle_venta AS det
+    JOIN cliente AS cli
+    ON det.id_cliente=cli.id_cleinte
+    JOIN provedor AS pro
+    ON det.id_provedor = pro.id_provedor
+    JOIN producto AS prod
+    ON det.id_producto = prod.id_producto";
+    if ($result = $conex->query($query1)) {
 
 		while ($row = $result->fetch_assoc()) {
-			$campo =$row["id_producto"];
-            $campo1 =$row["id_cliente"];
-            $id_prove = $row["id_provedor"];
+			$id_ped =$row["id_pedido"];
+            $cli_nom = $row["nombrecliente"];
+            $cli_apelli = $row["apellido"];
+            $cli_correo = $row["calleCLi"];
+            $cli_calle = $row["calleCLI"];
+            $cli_colonia = $row["coloniaCLi"];
+            $cli_edo = $row ["EDOcli"];
+            $cli_cel = $row ["celCLi"];
+            $cli_mail = $row["MAILcli"];
             $fecha = $row["fecha"];
-    		echo $campo. " " . $campo1;
-            echo $id_prove;
-            echo $fecha."<br>";
+            $nom_pro = $row["NOMpro"];
+            $rfc_prov = $row ["rfc_provedor"];
+            $nom_prov = $row["nombreproveedor"];
+            $calle_prov = $row["callePRO"];
+            $edo_pro = $row["EDOpro"];
+            $cel_pro = $row["CELpro"];
+    		echo $id_ped. " " . $cli_nom. "" . $cli_apelli. "" . $cli_calle;
+            echo $cli_cel. "" . $cli_colonia. "" . $cli_correo. "" . $cli_edo. "" . $cli_mail;
+            echo $fecha;
+            echo $nom_pro. "" . $rfc_prov. "" . $calle_prov. "" . $edo_pro. "" . $nom_prov;
+            echo $cel_pro."<br><br>";
             
 		}
 
