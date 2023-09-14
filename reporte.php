343558dc6bc825e5668c72589f85a90ecfe5777b
Polo The Noob <marr40201@gmail.com>
2023-09-14 00:14:52 -0600

    Reporte Ventas PDF Mostrar Datos

--- reporte.php
+++ reporte.php
@@ -4,6 +4,18 @@
     date_default_timezone_set('America/Mexico_City');
     $fechaACT = date ('Y-m-d');
 
+    $query1 = "SELECT det.id_pedido,det.unidades,cli.nombre as nombrecliente, cli.apellido,
+    cli.colonia AS calleCLI, cli.calle AS coloniaCLI, cli.estado AS EDOcli, cli.telefono AS celCLI,
+    cli.correo AS MAILcli, det.fecha, prod.nombre AS NOMpro, pro.rfc_provedor, pro.nombre as nombreproveedor,pro.colonia AS coloniaPRO,
+    pro.calle AS callePRO, pro.estado AS EDOpro, pro.telefono AS CELpro
+    FROM detalle_venta AS det
+    JOIN cliente AS cli
+    ON det.id_cliente=cli.id_cleinte
+    JOIN provedor AS pro
+    ON det.id_provedor = pro.id_provedor
+    JOIN producto AS prod
+    ON det.id_producto = prod.id_producto";
+
     $pdf = new FPDF('L');
     $pdf->AddPage();
     $pdf->SetFont('Arial','',14);
@@ -21,7 +33,23 @@
     $pdf->Cell(50,10,'Producto',1,0,'C');
     $pdf->Cell(25,10,'Unidades',1,0,'C');
     $pdf->Cell(80,10,'Cliente',1,0,'C');
-    $pdf->Cell(80,10,'Proveedor',1,0,'C');
+    $pdf->Cell(80,10,'Proveedor',1,1,'C');
+
+    $resultado = $conex->query($query1);
+
+        
+        foreach($resultado as $row){
+            $pdf->Cell(15,10,$row['id_pedido'],1,0);
+            $pdf->Cell(30,10,$row['fecha'],1,0);
+            $pdf->SetFont('Arial','',8);
+            $pdf->Cell(50,10,$row['NOMpro'],1,0);
+            $pdf->SetFont('Arial','',14);
+            $pdf->Cell(25,10,$row['unidades'],1,0,'C');
+            $pdf->SetFont('Arial','',12);
+            $pdf->Cell(80,10,$row['nombrecliente']."".$row['apellido'],1,0,'C');
+            $pdf->SetFont('Arial','',14);
+            $pdf->Cell(80,10,$row['nombreproveedor'],1,1,'C');
+        }
 
 
 
